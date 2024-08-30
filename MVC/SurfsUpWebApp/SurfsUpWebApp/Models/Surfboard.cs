dcee0ce367055ba9e038b5e745d771724d18dcc6
Greyrib <144381882+Greyrib@users.noreply.github.com>
2024-08-30 11:03:07 +0200

    Added hardcoded surfboards to also newly-added constructor for SurfboardRepo

--- MVC/SurfsUpWebApp/SurfsUpWebApp/Models/Surfboard.cs
+++ MVC/SurfsUpWebApp/SurfsUpWebApp/Models/Surfboard.cs
@@ -7,11 +7,26 @@
         public float Width { get; set; }
         public float Thickness {  get; set; }
         public float Volume { get; set; }
+
+        public BoardType boardType;
         public enum BoardType
         {
             None, Shortboard, Funboard, Fish, Longboard, SUP
         }
+
         public int Price { get; set; }
         public string Equipment { get; set; } // E.g. "Fin, Paddle, Pump, Leash"
+
+        public Surfboard (string boardName, float length, float width, float thickness, float volume, BoardType boardType, int price, string equipment)
+        {
+            BoardName = boardName;
+            Length = length;
+            Width = width;
+            Thickness = thickness;
+            Volume = volume;
+            this.boardType = boardType;
+            Price = price;
+            Equipment = equipment;
+        }   
     }
 }
