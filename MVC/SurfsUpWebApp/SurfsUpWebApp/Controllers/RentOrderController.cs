688ffae5582d49d1d383bdc58ce3d761f0aff412
Greyrib <144381882+Greyrib@users.noreply.github.com>
2024-09-03 11:25:10 +0200

    Added & updated form stuff, datepick & js-script

---
+++ MVC/SurfsUpWebApp/SurfsUpWebApp/Controllers/RentOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurfsUpWebApp.Controllers
+{
+    public class RentOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
