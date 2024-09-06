6d3034ad733ae33f194143b5b9616c9601b01756
Rasmus <rolfen@gmail.com>
2024-09-06 12:14:27 +0200

    minor updates. weekend time

---
+++ MVC/SurfsUpWebApp/SurfsUpWebApp/Controllers/BookingConfirmationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurfsUpWebApp.Controllers
+{
+    public class BookingConfirmationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
