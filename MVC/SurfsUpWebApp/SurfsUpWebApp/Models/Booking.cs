d8c467704a7babede562f0eb800d73cb9588f5ae
Rasmus <rolfen@gmail.com>
2024-09-10 09:39:15 +0200

    bookingclass created

--- MVC/SurfsUpWebApp/SurfsUpWebApp/Models/Booking.cs
+++ MVC/SurfsUpWebApp/SurfsUpWebApp/Models/Booking.cs
@@ -2,5 +2,28 @@
 {
     public class Booking
     {
+        public int BookingId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+        public string SelectedSurfboard { get; set; }
+        public int RentPeriod { get; set; }
+        public int RentHours { get; set; }
+        public Booking(int bookingId, string customerName, string customerEmail, string customerPhone, string selectedSurfboard, int rentPeriod, int rentHours)
+        {
+            BookingId = bookingId;
+            CustomerName = customerName;
+            CustomerEmail = customerEmail;
+            CustomerPhone = customerPhone;
+            SelectedSurfboard = selectedSurfboard;
+            RentPeriod = rentPeriod;
+            RentHours = rentHours;
+        }
+       
+
+
+
+
+
     }
 }
