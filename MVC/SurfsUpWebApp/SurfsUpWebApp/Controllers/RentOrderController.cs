d89039543935fd64a97c5aa1e0832cc328172e96
Michael Leicht Abildgaard <MLAbildgaard@live.dk>
2024-09-11 16:10:16 +0200

    Example Branch of Complex Model Passing

--- MVC/SurfsUpWebApp/SurfsUpWebApp/Controllers/RentOrderController.cs
+++ MVC/SurfsUpWebApp/SurfsUpWebApp/Controllers/RentOrderController.cs
@@ -5,9 +5,13 @@ namespace SurfsUpWebApp.Controllers
 {
     public class RentOrderController : Controller
     {
+        SurfboardRepository surfboardRepo = new SurfboardRepository();
+
         public IActionResult Index(string? passedSurfboard)
         {
-            return View(model: passedSurfboard); // Because if we pass a string into View() in MVC, it thinks we're looking for a View with that string as its name
+            var newBoard = surfboardRepo.Get_Surfboard_ByBoardName (passedSurfboard); // This is the way Frankie does it ; we have to create a new instance here, and pass it along below
+
+            return View(newBoard); // NOTE : If we pass a string into View() in MVC, it thinks we're looking for a View with that string as its name | If we need to do this, do 'View(model: myString)'
         }
     }
 }
