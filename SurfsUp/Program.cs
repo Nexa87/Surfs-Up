a84b86afe031c609044a5b049a2ff8b7fd5019d6
Greyrib <144381882+Greyrib@users.noreply.github.com>
2024-09-17 09:42:46 +0200

    Fixed routing in Program.cs

--- SurfsUp/Program.cs
+++ SurfsUp/Program.cs
@@ -5,9 +5,20 @@ namespace SurfsUp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", () => "Hello World!");
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
 
             app.Run();
         }
