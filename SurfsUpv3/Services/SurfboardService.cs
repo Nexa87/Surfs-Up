c760f46aaf44d19d1e510d220d9a1c564ce49376
jakob <jakobzden1@gmail.com>
2024-10-01 10:38:11 +0200

    Service folder made

---
+++ SurfsUpv3/Services/SurfboardService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Build.Construction;
+using SurfsUpv3.Migrations;
+using SurfsUpv3.Models;
+using System.Net.Http.Json;
+namespace SurfsUpv3.Services
+{
+    public class SurfboardService
+    {
+        private readonly HttpClient _httpclient;
+        public SurfboardService(HttpClient httpclient)
+        {
+            _httpclient = httpclient;
+        }
+
+        public async Task<IEnumerable<Surfboard>> GetSurfboardsAsync()
+        {
+            var response = await _httpclient.GetAsync("api/surfboards");
+            if (response.IsSuccessStatusCode)
+            {
+                var surfboards = await response.Content.ReadFromJsonAsync<IEnumerable<Surfboard>>("http://localhost:5055/weatherforecast/surfboard");
+                return surfboards;
+            }
+            else
+            {
+                throw new Exception("No surfboards found");
+            }
+
+            
+        }
+    }
+
+     
+}
