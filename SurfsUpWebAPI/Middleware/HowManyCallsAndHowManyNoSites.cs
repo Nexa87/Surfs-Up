4d179031808de6efffdc557d01669bf230f43a5b
Greyrib <144381882+Greyrib@users.noreply.github.com>
2024-10-04 12:19:34 +0200

    All SurfsUpv3 middleware thingies

---
+++ SurfsUpWebAPI/Middleware/HowManyCallsAndHowManyNoSites.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace SurfsUpWebAPI.Middleware
+{
+    public class HowManyCallsAndHowManyNoSites
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<HowManyCallsAndHowManyNoSites> _logger;
+        private static int _requestCount = 0;
+        private static int _notfoundcount = 0;
+
+        public HowManyCallsAndHowManyNoSites(RequestDelegate next, ILogger<HowManyCallsAndHowManyNoSites> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // If it's a 404, tally that too
+            if (context.Response.StatusCode == 404)
+                _notfoundcount += 1;
+
+            // Increment the request count
+            _requestCount++;
+
+            // Log the request count
+            _logger.LogInformation("API has been called {RequestCount} times.", _requestCount);
+            _logger.LogInformation($"404 requests: {_notfoundcount}");
+
+            // TODO Poke these info into the DB for future reference ?
+
+            // Call the next middleware in the pipeline
+            await _next(context);
+        }
+    }
+
+    // This permits us to do a 'app.UseHowManyHowMany();' in Program.cs :D
+    public static class HowManyCallsAndHowManyNoSitesExtensions
+    {
+        public static IApplicationBuilder UseHowManyHowMany(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HowManyCallsAndHowManyNoSites>();
+        }
+    }
+
+}
--- SurfsUpWebAPI/Program.cs
+++ SurfsUpWebAPI/Program.cs
@@ -1,3 +1,5 @@
+using SurfsUpWebAPI.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,12 +11,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//app.UseMiddleware<HowManyCallsAndHowManyNoSites>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHowManyHowMany(); // Custom not-pretty but recognizable :D
 
 app.UseHttpsRedirection();
 
