473dc724b327b86fc75a72c2de4356d183b7cbf9
Greyrib <144381882+Greyrib@users.noreply.github.com>
2024-10-13 15:43:51 +0200

    Update SurfsUpv3Context.cs
    SiteVisits not used ; remnant from earlier

--- SurfsUpWebAPI/Data/SurfsUpv3Context.cs
+++ SurfsUpWebAPI/Data/SurfsUpv3Context.cs
@@ -12,7 +12,6 @@ namespace SurfsUpWebAPI.Data
         public DbSet<Surfboard> Surfboards { get; set; } = default!;
         public DbSet<Booking> Bookings { get; set; } = default!;
         public DbSet<WetSuit> WetSuits { get; set; } = default!;
-        public DbSet<SiteVisit> SiteVisits { get; set; }
         public DbSet<SiteLog> SiteLogs { get; set; } // For logging 404 & API calls
         
         public SurfsUpv3Context (DbContextOptions<SurfsUpv3Context> options)
